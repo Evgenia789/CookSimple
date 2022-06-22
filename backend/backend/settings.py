eeb4c90c8a0e177980253a101be395d648a04930
Евгения Бакулина <jen.backulina@yandex.ru>
2022-06-22 21:10:17 +0300

    change ip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['178.154.198.206', 'localhost', 'myfoodgram.ddns.net']
+ALLOWED_HOSTS = ['84.201.138.231', 'localhost', 'myfoodgram.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
