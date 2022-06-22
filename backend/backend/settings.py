5bb5bcee3aba942e0e0ce17a0fa136fe270628ab
Евгения Бакулина <jen.backulina@yandex.ru>
2022-06-22 11:06:37 +0300

    change foodgram_workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost', 'backend']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
